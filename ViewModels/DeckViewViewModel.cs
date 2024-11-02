cb0014cb8e28979629ed4b556834fff6d923d3b5
KUBIXQAZ <kubajozwik20027@gmail.com>
2024-11-03 00:34:41 +0100

    Fixed error (When a user could play even tho he had no cards in the deck).

--- ViewModels/DeckViewViewModel.cs
+++ ViewModels/DeckViewViewModel.cs
@@ -20,22 +20,74 @@ namespace FlashLearn.ViewModels
             }
         }
 
+        private bool _errorVisibility = false;
+        public bool ErrorVisibility
+        {
+            get => _errorVisibility;
+            set
+            {
+                _errorVisibility = value;
+                OnPropertyChanged(nameof(ErrorVisibility));
+            }
+        }
+
+        private string _errorText;
+        public string ErrorText
+        {
+            get => _errorText;
+            set
+            {
+                _errorText = value;
+
+                if (_errorText != null) ErrorVisibility = true;
+                else ErrorVisibility = false;
+
+                OnPropertyChanged(nameof(ErrorText));
+            }
+        }
+
         public RelayCommand PlayCommand => new RelayCommand(execute => Play());
         public RelayCommand EditCommand => new RelayCommand(execute => EditDeck());
+        public RelayCommand ReturnCommand => new RelayCommand(execute => Return());
+
+        private void Return()
+        {
+            Shell.Current.Navigation.PopAsync();
+        }
 
         public DeckViewViewModel(DeckModel deck)
         {
             _deck = deck;
 
             DeckName = _deck.Name;
+
+            if(!CheckForCards())
+            {
+                ErrorText = "You can't play, there is no cards in this deck!";
+            }
+        }
+
+        private bool CheckForCards()
+        {
+            if(_deck.Cards.Count == 0)
+            {
+                return false;
+            } 
+            else
+            {
+                return true;
+            }
         }
 
         private void Play()
         {
-            GamePage gamePage = new GamePage();
-            GameViewModel gameViewModel = new GameViewModel(_deck);
-            gamePage.BindingContext = gameViewModel;
-            Shell.Current.Navigation.PushAsync(gamePage);
+            if(CheckForCards())
+            {
+                GamePage gamePage = new GamePage();
+                GameViewModel gameViewModel = new GameViewModel(_deck);
+                gamePage.BindingContext = gameViewModel;
+                Shell.Current.Navigation.PushAsync(gamePage);
+            }
         }
 
         private void EditDeck()
